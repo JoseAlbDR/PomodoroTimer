e0e603a22f24346741b6e61264f6011f971ef06a
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-04-14 12:21:17 +0200

    UI Setup completed with Canvas and PhotoImage

--- main.py
+++ main.py
@@ -17,3 +17,18 @@ LONG_BREAK_MIN = 20
 
 # ---------------------------- UI SETUP ------------------------------- #
 
+window = Tk()
+window.title("Pomodoro")
+window.config(padx=100, pady=50, bg=YELLOW)
+
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+tomato_img = PhotoImage(file="tomato.png")
+
+canvas.create_image(100, 112, image=tomato_img)
+canvas.create_text(100, 132, text="00:00", fill="white", font=(FONT_NAME, 28, "bold"))
+
+canvas.pack()
+
+
+
+window.mainloop()
