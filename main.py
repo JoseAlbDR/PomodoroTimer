b0500db7be64f8d7e4af9a61465dcea72ff98299
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-04-14 12:44:29 +0200

    Added timer and check text and Start and Reset buttons

--- main.py
+++ main.py
@@ -17,17 +17,39 @@ LONG_BREAK_MIN = 20
 
 # ---------------------------- UI SETUP ------------------------------- #
 
+# Window
 window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
 
+# Check
+check = "✓"
+
+# Canvas
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
-
 canvas.create_image(100, 112, image=tomato_img)
-canvas.create_text(100, 132, text="00:00", fill="white", font=(FONT_NAME, 28, "bold"))
+canvas.create_text(100, 132, text="00:00", fill="white", font=(FONT_NAME, 32, "bold"))
+canvas.grid(column=1, row=1)
 
-canvas.pack()
+# Text labels
+timer_label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 28, "bold"))
+timer_label.grid(column=1, row=0)
+
+check_label = Label(text=check, fg=GREEN, bg=YELLOW)
+check_label.grid(column=1, row=3)
+
+# Buttons
+start_button = Button(text="Start")
+start_button.grid(row=2, column=0)
+
+reset_button = Button(text="Reset")
+reset_button.grid(row=2, column=2)
+
+
+
+
+
 
 
 
