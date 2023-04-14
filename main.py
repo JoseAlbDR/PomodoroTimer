76010171635bf7e37da15de4fd83d90953c135c1
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-04-14 13:35:16 +0200

    Added timer mechanism and countdown mechanism

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -9,11 +10,32 @@ WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 
-# ---------------------------- TIMER RESET ------------------------------- # 
+
+# ---------------------------- TIMER RESET ------------------------------- #
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+def start_timer():
+    count = WORK_MIN * 60
+    count_down(count)
+
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+
+def count_down(count):
+    count_minute = int(count / 60)
+    count_seconds = count % 60
+
+    if len(str(count_seconds)) == 1:
+        format_seconds = f"0{count_seconds}"
+    else:
+        format_seconds = count_seconds
+
+    timer = f"{count_minute}:{format_seconds}"
+    canvas.itemconfig(timer_text, text=timer)
+    if count > 0:
+        window.after(1000, count_down, count - 1)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -29,7 +51,7 @@ check = "✓"
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
-canvas.create_text(100, 132, text="00:00", fill="white", font=(FONT_NAME, 32, "bold"))
+timer_text = canvas.create_text(100, 132, text="00:00", fill="white", font=(FONT_NAME, 32, "bold"))
 canvas.grid(column=1, row=1)
 
 # Text labels
@@ -40,17 +62,10 @@ check_label = Label(text=check, fg=GREEN, bg=YELLOW)
 check_label.grid(column=1, row=3)
 
 # Buttons
-start_button = Button(text="Start")
+start_button = Button(text="Start", command=start_timer)
 start_button.grid(row=2, column=0)
 
 reset_button = Button(text="Reset")
 reset_button.grid(row=2, column=2)
-
-
-
-
-
-
-
 
 window.mainloop()
